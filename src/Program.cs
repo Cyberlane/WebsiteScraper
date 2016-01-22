3fe754229375b636cd9db2634f5ee55bdb0fa0bd
Cyberlane <dogbertuk2000@gmail.com>
2016-01-22 09:47:20 +0100

    Fetch the website URL from the user

--- src/Program.cs
+++ src/Program.cs
@@ -10,6 +10,47 @@ namespace WebsiteScraper
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("#####################");
+            Console.WriteLine("#  Website Scraper  #");
+            Console.WriteLine("#####################");
+            Console.WriteLine();
+            var uri = GetWebsiteUri();
+            Console.WriteLine("Fetching: " + uri);
+            Console.ReadKey();
+        }
+
+        private static Uri GetWebsiteUri()
+        {
+            while (true)
+            {
+                Console.Write("Enter website URL: ");
+                var url = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(url))
+                {
+                    Console.WriteLine("Error: Please enter a URL.");
+                    continue;
+                }
+
+                Uri uri;
+                if (!Uri.TryCreate(url, UriKind.Absolute, out uri))
+                {
+                    Console.WriteLine("Error: Invalid URL.");
+                    continue;
+                }
+
+                if (!uri.IsWellFormedOriginalString())
+                {
+                    Console.WriteLine("Error: Invalid URL.");
+                    continue;
+                }
+
+                if (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps)
+                {
+                    return uri;
+                }
+
+                Console.WriteLine("Error: Prefix your URL with HTTP:// or HTTPS://");
+            }
         }
     }
 }
